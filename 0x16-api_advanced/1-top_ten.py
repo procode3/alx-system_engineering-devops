40d4656b2c791794acef3ed1fd656bef15dc9c72
procode3 <wr.t8r1@gmail.com>
2023-05-09 15:08:21 +0300

    Geting 10 hot posts on a subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+
+
+def top_ten(subreddit):
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    headers = {"User-Agent": "Custom User"}
+    params = {"limit": 10}
+    res = requests.get(url, headers=headers, params=params)
+    data = res.json().get("data").get(
+           "children") if res.status_code == 200 else 0
+    for _ in data:
+        print(_.get("data").get("title"))
+    return data
